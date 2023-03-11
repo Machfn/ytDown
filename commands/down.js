4468128f00538d6262955551759a7ff55106ecb5
Benon <66958303+Machfn@users.noreply.github.com>
2023-03-10 21:32:16 -0700

    updating for linux support

--- commands/down.js
+++ commands/down.js
@@ -1,6 +1,7 @@
 const ytdl = require('ytdl-core');
 const fs = require('fs');
-let folder = process.env.userprofile + '/downloads';
+const os = require('os')
+let folder = os.homedir() + '/Downloads';
 
 
 async function downy(fileTitle, url) {
@@ -8,7 +9,7 @@ async function downy(fileTitle, url) {
     ytdl(url, {filter: 'audioonly'})
     .pipe(fs.createWriteStream(`${folder}/${fileTitle}.mp3`))
     .on('error', (err) => {
-        // console.log(err)
+        console.log(err)
     })
     .on('finish', () => {
         console.log('All Done');
